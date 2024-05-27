9e39bf197ee36dd1998f677facdbed0ce93d82b6
m.ferdosian <m.ferdosian@asanpardakht.com>
2024-05-27 15:27:56 +0330

    add new file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ferdosian93/puppy
+
+go 1.21.4
---
+++ puppy.go
@@ -0,0 +1,10 @@
+package puppy
+
+import "fmt"
+
+func Bark() {
+	fmt.Println("hup!!!")
+}
+func Barks() {
+	fmt.Println("ups ups !!!")
+}
